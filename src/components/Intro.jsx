105fbb515fbf2d205e92e74a8c9c28ad183c66b2
ho8ae <xogh2242@gmail.com>
2024-02-14 18:13:01 +0900

    Intro complete :)

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,8 +1,47 @@
 import React from "react";
+import about from "../assets/img/about.png";
+
+const introText={
+    title:"X-TRIC",
+    desc:["talent is","found at the end of the","effort"],
+};
 
 const Intro = () =>{
-    return <div>Intro</div>
-};
+    return(
+    <section id = "intro">
+        <div class = "intro__inner">
+            <h1 class="intro__title">{introText.title}</h1>
+            <div class="intro__lines" aria-hidden="true">
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+            </div>
+            <div class="intro__text">
+                <div class= "text">
+                    <div>{introText.desc[0]}</div>
+                    <div>{introText.desc[1]}</div>
+                    <div>{introText.desc[2]}</div>
+                </div>
+                <div class = "img">
+                    <img src= {about} alt = "어바웃"/>
+                </div>
+            </div>
+            <div class = "intro__lines bottom" aria-hidden="true">
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                </div>
+        </div>
+    </section>
+)};
 
 export default Intro;
 
